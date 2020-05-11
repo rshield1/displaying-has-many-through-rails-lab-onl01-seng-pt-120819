cd1e7e86191488c47202d4f339422d07fc1f2dea
Robert Shields <robert.shieldsiv@gmail.com>
2020-05-11 01:02:34 +0000

    Done.

--- app/models/appointment.rb
+++ app/models/appointment.rb
@@ -1,4 +1,26 @@
 class Appointment < ApplicationRecord
-    belongs_to :doctor
-    belongs_to :patient
+  belongs_to :doctor
+  belongs_to :patient
+
+  def doctor_name=(name)
+     self.doctor = Doctor.find_or_create_by(name: name)
+   end
+
+   def doctor_name
+      self.doctor ? self.doctor.name : nil
+   end
+
+  def patient_name=(name)
+    self.patient = Patient.find_or_create_by(name: name)
+  end
+
+  def patient_name
+    self.patient ? self.patient.name : nil
+  end 
+  
+  def timeslot
+    appointment_datetime.strftime('%B %d, %Y at %k:%M')
+  end
+  
+  
 end
